f6714dccc8866a5cfcc5806493e928dcca0a1cb4
soloprojects <solomonnweze@gmail.com>
2019-09-06 21:08:33 +0100

    Work ongoing on CRM Modules(Created Migrations,Models,Controllers

---
+++ app/Http/Controllers/CrmStagesController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CrmStagesController extends Controller
+{
+    //
+}
---
+++ database/migrations/2019_09_06_191354_create_crm_stages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCrmStagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crm_stages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crm_stages');
+    }
+}
